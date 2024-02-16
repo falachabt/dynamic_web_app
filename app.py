d38e821e89d67f73ebe6e5ee17c8aebdf235ceb3
falachabt <bennytenezeu@gmail.com>
2024-02-16 16:34:42 +0100

    students_choices ok

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
+
 app = Flask(__name__)
 
 
@@ -16,7 +17,8 @@ def database():
     cur = mysql.connection.cursor()
 
     # Execute the SQL query to select data from the table
-    cur.execute("SELECT studentMail, campusName FROM mobilitywish JOIN campus ON mobilitywish.Campus_idCampus = campus.idCampus")
+    cur.execute("SELECT * FROM campus")
+    # cur.execute("SELECT studentMail, campusName FROM mobilitywish JOIN campus ON mobilitywish.Campus_idCampus = campus.idCampus")
 
     # Fetch all the results
     rows = cur.fetchall()
@@ -30,6 +32,40 @@ def database():
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+
+@app.route('/choices')
+def choices():
+    search_query = request.args.get('search')
+
+    # Initialize the cursor before using it
+    cur = mysql.connection.cursor()
+
+    if search_query:
+        # Use parameterized queries to prevent SQL injection
+        query = '''
+            SELECT studentMail, campusName 
+            FROM mobilitywish 
+            JOIN campus ON mobilitywish.Campus_idCampus = campus.idCampus 
+            WHERE mobilitywish.studentMail LIKE %s 
+            OR campus.campusName LIKE %s ORDER BY studentMail   
+        '''
+        search_term = '%' + search_query + '%'
+        cur.execute(query, (search_term, search_term))
+    else:
+        cur.execute('''
+            SELECT studentMail, campusName 
+            FROM mobilitywish 
+            JOIN campus ON mobilitywish.Campus_idCampus = campus.idCampus ORDER BY studentMail 
+        ''')
+
+    # Fetch the results after executing the query
+    wishes = cur.fetchall()
+
+    # Close the cursor after fetching the results
+    cur.close()
+
+    return render_template('choices.html', choices=wishes, search = search_query)
 
 
 @app.route('/login')
